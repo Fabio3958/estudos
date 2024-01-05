4dc7b5d2d4f5158b5b482e5421f179ddab83d35f
Fabio3958 <fabioevangelista_@hotmail.com>
2024-01-05 16:53:28 -0300

    Code Wars - Shortest Word

---
+++ CodeWars/ShortestWord.java
@@ -0,0 +1,24 @@
+package CodeWars;
+
+import java.util.Arrays;
+
+public class ShortestWord {
+
+    //Simple, given a string of words, return the length of the shortest word(s).
+    //String will never be empty
+    //you do not need to account for different data types.
+
+
+    public static int findShort(String s) {
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(findShort("ce ta loco"));
+
+    }
+
+}
